bc9581e06afb34bbc01504e7e65dae1ec077d865
liuhu016 <liuhu016@realsee.com>
2024-08-21 08:41:27 +0800

    update

--- post.sh
+++ post.sh
@@ -1,13 +1,23 @@
 #!/bin/bash
 
+if [ $# -lt 2 ]; then
+    echo "Usage: $0 <category> <title>"
+    exit
+fi
 CATEGORY=$1
+TITLE=$2
 SLUG=$(date +%Y-%m-%d-%H-%M-%S)
 DATE=$(date +%Y-%m-%d)
 mkdir -p ./content/post/$CATEGORY/$SLUG
 
-YAML="---\ntitle: YourTitle\ndate: ${DATE}\nslug: ${SLUG}\ncategories:\n  - ${CATEGORY}\ntags:\n  - xxx\n---\n"
+YAML=$(cat content.template)
+cp content.template ./content/post/$CATEGORY/$SLUG/index.md
 
-echo -ne $YAML > ./content/post/$CATEGORY/$SLUG/index.md
+sed -i 's/SLUG/'${SLUG}'/g' ./content/post/$CATEGORY/$SLUG/index.md
+sed -i 's/DATE/'${DATE}'/g' ./content/post/$CATEGORY/$SLUG/index.md
+sed -i 's/CATEGORY/'${CATEGORY}'/g' ./content/post/$CATEGORY/$SLUG/index.md
+sed -i 's/TITLE/'${TITLE}'/g' ./content/post/$CATEGORY/$SLUG/index.md
+
 
 echo "File created: ./content/post/$CATEGORY/$SLUG/index.md"
-code ./content/post ./content/post/$CATEGORY/$SLUG/index.md
+code . ./content/post/$CATEGORY/$SLUG/index.md
